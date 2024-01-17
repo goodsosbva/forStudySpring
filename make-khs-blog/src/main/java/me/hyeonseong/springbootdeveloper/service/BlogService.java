adfa626135fdf7560a7517fd329083613e10cb8b
goodsosbva@naver.com <hs.kwan@okestro.com>
2024-01-18 00:54:36 +0900

    [feat] R,U,D 블로그 기능 구현 완료 + with testcode

--- make-khs-blog/src/main/java/me/hyeonseong/springbootdeveloper/service/BlogService.java
+++ make-khs-blog/src/main/java/me/hyeonseong/springbootdeveloper/service/BlogService.java
@@ -3,8 +3,10 @@ package me.hyeonseong.springbootdeveloper.service;
 import lombok.RequiredArgsConstructor;
 import me.hyeonseong.springbootdeveloper.domain.Article;
 import me.hyeonseong.springbootdeveloper.dto.AddArticleRequest;
+import me.hyeonseong.springbootdeveloper.dto.UpdateArticleRequest;
 import me.hyeonseong.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,5 +23,24 @@ public class BlogService {
 
     public List<Article> findAll() {
         return blogRepository.findAll();
+    }
+
+    public Article findById(long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+    }
+
+    public void delete(long id) {
+        blogRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 }
